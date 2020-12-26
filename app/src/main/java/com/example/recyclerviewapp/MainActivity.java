41b351c4c0ad0b1b72f6ef44bc084729801d812c
Mikhail Buimister <elevis567@gmail.com>
2020-12-26 21:03:19 +0700

    Добавлен класс RViewAdapter

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.recyclerviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+
+
     }
+
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.list);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        DummyContent dummyContent = new DummyContent();
+
+
+        RecyclerView.Adapter adapter = new RViewAdapter(dummyContent.ITEMS);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+
 }
+
